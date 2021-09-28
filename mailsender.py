19655f4e9840fbca1c210c2bb11926a70310f79e
Marek_p <marek.mnp@seznam.cz>
2021-09-28 12:38:39 +0200

    Update mailsender.py

--- mailsender.py
+++ mailsender.py
@@ -9,12 +9,12 @@ print("---------------------------")
 
 sender_email = "váš gmail"
 rec_email = input(str("Email Příjemce: "))
-password = input(str("Heslo Odesílatele: "))
-message = input(str("Zpráva: "))
+password = input(str("Heslo Odesílatele: ")) # Zeptá se vás na heslo k "sender_mail"
+message = input(str("Zpráva: ")) # Zeptá se vás na zprávu
 
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login(sender_email, password)
+server = smtplib.SMTP('smtp.gmail.com', 587) # Adresa a port mail serveru
+server.starttls() # Vytvoření spojení
+server.login(sender_email, password) # Přihlášení
 print("Úspěšně přihlášen")
-server.sendmail(sender_email, rec_email, message)
+server.sendmail(sender_email, rec_email, message) # Odeslání mailu
 print("Email byl poslán na", rec_email)
